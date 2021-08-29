6dee13c05416d595ba0fea32f6f89b7f98a25488
Mikhail <km61532@gmail.com>
2021-08-30 01:35:18 +0300

    Task done.

--- src/main/java/ru/geekbrains/lesson7/Cat.java
+++ src/main/java/ru/geekbrains/lesson7/Cat.java
@@ -4,6 +4,7 @@ public class Cat {
 
     private String name;
     private int appetite;
+    private boolean satiety;
 
     public Cat (String name, int appetite) {
 
@@ -15,11 +16,11 @@ public class Cat {
     public boolean catEat (PlateWithFood plateWithFood) {
         if (plateWithFood.foodAmount < appetite) {
             System.out.println(name + "у не хватает еды в миске. Кот не ест.");
-            return false;
+            return satiety = false;
         } else {
             plateWithFood.foodAmount -= appetite;
             System.out.println(name + " поел. Кот сыт.");
-            return true;
+            return satiety = true;
         }
     }
 }
