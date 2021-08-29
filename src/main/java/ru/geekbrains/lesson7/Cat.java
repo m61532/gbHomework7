d9655d89c568d83e29b87a9dd0a6789cb890f241
Mikhail <km61532@gmail.com>
2021-08-30 01:40:59 +0300

    Task done. Minor additions.

--- src/main/java/ru/geekbrains/lesson7/Cat.java
+++ src/main/java/ru/geekbrains/lesson7/Cat.java
@@ -1,5 +1,7 @@
 package ru.geekbrains.lesson7;
 
+import java.util.Objects;
+
 public class Cat {
 
     private String name;
@@ -22,5 +24,39 @@ public class Cat {
             System.out.println(name + " поел. Кот сыт.");
             return satiety = true;
         }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAppetite() {
+        return appetite;
+    }
+
+    public void setAppetite(int appetite) {
+        this.appetite = appetite;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cat)) return false;
+        Cat cat = (Cat) o;
+        return appetite == cat.appetite && satiety == cat.satiety && Objects.equals(name, cat.name);
+    }
+
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(name, appetite, satiety);
+//    }
+
+    @Override
+    public String toString() {
+        return "Кот по кличке " + name;
     }
 }
